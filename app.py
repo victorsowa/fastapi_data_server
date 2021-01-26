9c923e22a1ddbbaa9346a4916741698520c2b091
Victor Sowa <victor.sowa@etraveligroup.com>
2021-01-26 21:56:40 +0100

    Move parsing of csv to helpers.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 import os
-import json
 
 from fastapi import FastAPI
-import pandas as pd
 
-from scripts.helpers import get_last_modified_time
+from scripts.helpers import (get_last_modified_time,
+                             parse_csv)
 
 
 app = FastAPI()
@@ -19,11 +18,10 @@ def get_available_data():
 @app.get("/get_data/{filename}")
 def get_data(filename):
     file_path = os.path.join(DATA_DIRECTORY, filename)
-    df_from_file = pd.read_csv(file_path)
-    df_json_string = df_from_file.to_json()
+    parsed_csv = parse_csv(file_path)
 
     modified_time = get_last_modified_time(file_path)
 
     return {
         'lastModified': modified_time,
-        'data': json.loads(df_json_string)}
+        'data': parsed_csv}
